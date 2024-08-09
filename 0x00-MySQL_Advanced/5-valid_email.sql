1dcef68954e5582346be978346169aa88ef41160
zguaidh <zguaidhabiba@gmail.com>
2024-08-09 00:16:15 +0000

    tasks

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,9 +1,14 @@
 -- Creates a trigger to reset 'valid_email' when email is changed
-CREATE TRIGGER reset_valid_email BEFORE UPDATE ON users
+DELIMITER //
+
+CREATE TRIGGER reset_valid_email
+BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF OLD.email != NEW.email THEN
+    IF NEW.email != OLD.email THEN
         SET NEW.valid_email = 0;
     END IF;
-END;
+END //
+
+DELIMITER ;
 
