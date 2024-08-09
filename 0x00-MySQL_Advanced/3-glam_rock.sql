3512da49b5d066a1c9874822521ccfc5cc5da450
zguaidh <zguaidhabiba@gmail.com>
2024-08-09 00:33:16 +0000

    tasks

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,8 @@
 -- Lists all Glam rock bands ranked by longevity
 SELECT band_name, 
        CASE 
-           WHEN split IS NULL THEN (2022 - formed)
-           ELSE (split - formed)
+           WHEN split IS NOT NULL THEN (split - formed)
+           ELSE (2022 - formed)
        END AS lifespan
 FROM metal_bands
 WHERE style = 'Glam rock'
