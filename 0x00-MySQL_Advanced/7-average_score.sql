6bec0fbbb2ed6538e710f08a7d96feefb2d50ecb
zguaidh <zguaidhabiba@gmail.com>
2024-08-09 00:07:35 +0000

    tasks

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,16 +1,15 @@
 -- Creates a procedure 'ComputeAverageScoreForUser' to compute the average score
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN in_user_id INT)
 BEGIN
     DECLARE avg_score FLOAT;
 
     SELECT AVG(score) INTO avg_score
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE user_id = in_user_id;
 
-
-    UPDATE users SET average_score = avg_score WHERE id = user_id;
+    UPDATE users SET average_score = avg_score WHERE id = in_user_id;
 END //
 
 DELIMITER ;
