54461841e9d802d0f043f016f0ff9e32c0a361db
zguaidh <zguaidhabiba@gmail.com>
2024-08-13 15:41:58 -0700

    task10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Changes all topics of a school document based on the name'''
+
+
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name, topics):
+    mongo_collection.update_many(
+        { "name": name },
+        { "$set": { "topics": topics } }
+    )
