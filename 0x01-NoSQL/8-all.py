1b5cb06e7a9f404058036ca98cb1356048674ed1
zguaidh <zguaidhabiba@gmail.com>
2024-08-13 14:59:11 -0700

    task8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -4,11 +4,11 @@
 
 from pymongo import MongoClient
 
-
-def list_all(mongo_collection):
-    '''Returns all documents in a collection'''
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    doc = []
-    for doc in client.my_db.mango_collection.find()
-        doc.append(client.my_db.mango_collection.find())
-    return doc
+if __name__ == "__main__":
+    def list_all(mongo_collection):
+        '''Returns all documents in a collection'''
+        client = MongoClient('mongodb://127.0.0.1:27017')
+        doc = []
+        for doc in client.my_db.mango_collection.find()
+            doc.append(client.my_db.mango_collection.find())
+        return doc
